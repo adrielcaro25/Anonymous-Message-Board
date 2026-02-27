97d38297b646dfe9ced7495917bcb4998e434ae3
adrielcaro25 <adrielefernandezcaro10@gmail.com>
2026-02-27 18:30:07 +0000

    add: add files required

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,10 @@
-'use strict';
-
+"use strict";
+const routerThreads = require("../controllers/thread.controller.js");
+const routerReplies = require("../controllers/reply.controller.js");
+const express = require("express");
+const router = express.Router();
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+  app.use("/", routerThreads(router));
 
+  app.use("/", routerReplies(router));
 };
